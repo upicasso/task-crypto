a304eafe3ddcbe236b39e9c2c3bb10fdf8e0706e
Vasyl Tarasov <u0699865203@gmail.com>
2026-02-27 21:27:22 +0100

    Basic controller test edited

--- app/tests/Controller/PortfolioControllerTest.php
+++ app/tests/Controller/PortfolioControllerTest.php
@@ -6,11 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 final class PortfolioControllerTest extends WebTestCase
 {
-    public function testHistory(): void
+    public function testHistoryReturnsValidJson(): void
     {
         $client = static::createClient();
         $client->request('GET', '/api/portfolio/history');
 
         self::assertResponseIsSuccessful();
+        self::assertResponseHeaderSame('content-type', 'application/json');
+
+        $data = json_decode($client->getResponse()->getContent(), true, 512, JSON_THROW_ON_ERROR);
+
+        self::assertIsArray($data);
+
+        if ($data !== []) {
+            self::assertArrayHasKey('time', $data[0]);
+            self::assertArrayHasKey('amount_usdt', $data[0]);
+        }
     }
 }
