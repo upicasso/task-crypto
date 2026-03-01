5f76222ef774f4ccbe4efb1e727deba1644f7753
Vasyl Tarasov <u0699865203@gmail.com>
2026-03-01 12:08:16 +0100

    Added PHPDocblocks to PortfolioValue.php

--- app/src/Entity/PortfolioValue.php
+++ app/src/Entity/PortfolioValue.php
@@ -17,27 +17,46 @@ class PortfolioValue
      */
     const PORTFOLIO_VALUE_CURRENCY = 'USDT';
 
+    /**
+     * @var int|null
+     */
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
+    /**
+     * @var \DateTimeImmutable|null
+     */
     #[ORM\Column]
     private ?\DateTimeImmutable $calculatedAt = null;
 
+    /**
+     * @var string|null
+     */
     #[ORM\Column(type: Types::DECIMAL, precision: 20, scale: 10)]
     private ?string $amountUsdt = null;
 
+    /**
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * @return \DateTimeImmutable|null
+     */
     public function getCalculatedAt(): ?\DateTimeImmutable
     {
         return $this->calculatedAt;
     }
 
+    /**
+     * @param \DateTimeImmutable $calculatedAt
+     * @return $this
+     */
     public function setCalculatedAt(\DateTimeImmutable $calculatedAt): static
     {
         $this->calculatedAt = $calculatedAt;
@@ -45,11 +64,18 @@ class PortfolioValue
         return $this;
     }
 
+    /**
+     * @return Money
+     */
     public function getAmountUsdt(): Money
     {
         return new Money($this->amountUsdt, new Currency(self::PORTFOLIO_VALUE_CURRENCY));
     }
 
+    /**
+     * @param Money $amountUsdt
+     * @return $this
+     */
     public function setAmountUsdt(Money $amountUsdt): static
     {
         $this->amountUsdt = $amountUsdt->getAmount();
